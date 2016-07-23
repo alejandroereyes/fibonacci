251595dadf6615774499b24f325cc7945fcfe47a
Alex Reyes <alejandroereyes@gmail.com>
2016-07-23 18:19:12 -0500

    added recursive method of getting n numbers of the fibonacci sequence

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,17 +1,32 @@
+require 'pry'
 module Fibonacci
-  # Both methods return to get that many
-  # numbers of the Fibonacci sequence
+  # Both methods return n many numbers
+  # of the Fibonacci sequence
   module_function
 
   def fibs(n)
     # iterative soluion
+    return if n < 1
     arr = [0, 1]
+    return arr.first if n == 1
+    return arr       if n == 2
 
-    (n - 1).times do
+    (n - 2).times do
       arr << arr[-2] + arr[-1]
     end
     arr
   end
+
+  def fibs_recur(n, arr=[0, 1])
+    # recursive solution
+    return           if n < 1
+    return arr.first if n == 1
+    return arr       if n == 2
+
+    arr << arr[-2] + arr[-1]
+    fibs_recur n -1, arr
+  end
 end
-print "Fibonacci.fibs 10 # => "
-puts Fibonacci.fibs 10
+puts "Fibonacci.fibs 10 # => #{Fibonacci.fibs 10}"
+
+puts "Fibonacci.fibs_recur 10 # => #{Fibonacci.fibs_recur 10}"
